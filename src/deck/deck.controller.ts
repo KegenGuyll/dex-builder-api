29701bc29adac546d6c50959ffe9b31c8d58006e
Kegen Guyll <guyllkegen@gmail.com>
2024-08-11 18:41:56 -0500

    chore: Add authentication decorator and guard for deck routes

--- src/deck/deck.controller.ts
+++ src/deck/deck.controller.ts
@@ -11,12 +11,14 @@ import { DeckService } from './deck.service';
 import { Deck } from './interface/deck.interface';
 import { CreateDeckDto } from './dto/create-deck.dto';
 import { ValidationPipe } from 'src/common/pipes/validation.pipe';
+import { Auth } from 'src/decorators/auth.decorator';
 
 @Controller('deck')
 export class DeckController {
   constructor(private deckService: DeckService) {}
 
   @Post()
+  @Auth('USER')
   async create(
     @Body(new ValidationPipe()) createDeckDto: CreateDeckDto,
   ): Promise<string> {
@@ -26,16 +28,19 @@ export class DeckController {
   }
 
   @Get()
+  @Auth('USER')
   findAll(): Promise<Deck[]> {
     return this.deckService.findAll();
   }
 
   @Get(':deckId')
+  @Auth('USER')
   findOne(@Param('deckId') deckId: string): Promise<Deck> {
     return this.deckService.findOne(deckId);
   }
 
   @Put(':deckId')
+  @Auth('USER')
   update(
     @Param('deckId') deckId: string,
     @Body(new ValidationPipe()) updateDeckDto: CreateDeckDto,
@@ -44,6 +49,7 @@ export class DeckController {
   }
 
   @Delete(':deckId')
+  @Auth('USER')
   remove(@Param('deckId') deckId: string): Promise<string> {
     return this.deckService.remove(deckId);
   }
--- src/deck/deck.module.ts
+++ src/deck/deck.module.ts
@@ -3,13 +3,14 @@ import { DeckController } from './deck.controller';
 import { DeckService } from './deck.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Deck, DeckSchema } from 'src/schemas/deck.schema';
+import { FirebaseAdmin } from 'config/firebase.setup';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Deck.name, schema: DeckSchema }]),
   ],
   controllers: [DeckController],
-  providers: [DeckService],
+  providers: [DeckService, FirebaseAdmin],
   exports: [DeckService],
 })
 export class DeckModule {}
